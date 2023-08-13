084f065f3d610c1c243c9d6714cabf126e4e276e
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-08-13 11:30:11 +1000

    Update update.py

--- stable/update.py
+++ stable/update.py
@@ -1,18 +1,21 @@
 import os, sys, requests
-getNewVersion = requests.get("https://raw.githubusercontent.com/JustAnEric/amika/main/stable/version").text()
+getNewVersion = requests.get("https://raw.githubusercontent.com/JustAnEric/amika/main/stable/version").text
 with open('./version', 'r') as f:
   versionDetected = f.readlines()[0]
   if versionDetected.split(' ')[0] == getNewVersion.split(' ')[0]:
     print("You are using the latest version.")
   else:
     print("We are downloading the latest version...")
-    getNewMainFile = requests.get("https://raw.githubusercontent.com/JustAnEric/amika/main/stable/main.py").text()
+    getNewMainFile = requests.get("https://raw.githubusercontent.com/JustAnEric/amika/main/stable/main.py").text
     with open('./main.py','w') as f:
       f.write(getNewMainFile)
       f.close()
-    getNewDataset = requests.get("https://raw.githubusercontent.com/JustAnEric/amika/main/stable/dataset.yml").text()
+    getNewDataset = requests.get("https://raw.githubusercontent.com/JustAnEric/amika/main/stable/dataset.yml").text
     with open('./dataset.yml', 'w') as f:
       f.write(getNewDataset)
+      f.close()
+    with open('./version', 'w') as f:
+      f.write(getNewVersion)
       f.close()
     print("Wrote new dataset.")
     print("Updated successfully, reboot?")
